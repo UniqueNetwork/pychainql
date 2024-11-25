c34c370b82e1460e2007a47998a97fc7e4f93ba9
Valery Klachkov <zgdump@gmail.com>
2024-11-25 23:14:55 +0100

    Make JsonnetObject and JsonnetArray convenient collections

--- src/jsonnet.rs
+++ src/jsonnet.rs
@@ -13,7 +13,7 @@
 // limitations under the License.
 
 use crate::{
-    jsonnet_py::{jsonnet_to_py, pylist_to_jsonnet},
+    jsonnet_py::{jsonnet_to_py, py_to_jsonnet, pylist_to_jsonnet},
     jsonnet_tokio::execute_jsonnet,
     utils::{jsonnet_error, type_error},
 };
@@ -22,6 +22,7 @@ use pyo3::{
     prelude::*,
     types::PyTuple,
 };
+use std::cell::RefCell;
 
 /// TODO
 #[pyclass(unsendable, mapping)]
@@ -30,13 +31,23 @@ pub struct JsonnetObject(pub jrsonnet_evaluator::ObjValue);
 
 #[pymethods]
 impl JsonnetObject {
-    fn __getitem__<'py>(
+    fn __len__(&self) -> usize {
+        self.0.len()
+    }
+
+    fn __contains__(&self, key: &Bound<'_, PyAny>) -> bool {
+        key.extract::<&str>()
+            .map(|key| self.0.has_field(key.into()))
+            .unwrap_or(false)
+    }
+
+    fn __getattr__<'py>(
         &self,
         py: Python<'py>,
-        key: &Bound<'_, PyAny>,
+        name: &Bound<'_, PyAny>,
     ) -> PyResult<Bound<'py, PyAny>> {
         execute_jsonnet(|| {
-            let key = key
+            let key = name
                 .extract::<&str>()
                 .map_err(|err| type_error(py, "key should be a string", err))?;
 
@@ -50,7 +61,37 @@ impl JsonnetObject {
         })
     }
 
-    /// TODO
+    fn __getitem__<'py>(
+        &self,
+        py: Python<'py>,
+        key: &Bound<'_, PyAny>,
+    ) -> PyResult<Bound<'py, PyAny>> {
+        self.__getattr__(py, key)
+    }
+
+    #[pyo3(signature = (include_hidden=false))]
+    fn keys(&self, include_hidden: bool) -> JsonnetObjectKeys {
+        JsonnetObjectKeys {
+            iter: RefCell::new(Box::new(self.0.fields_ex(include_hidden, true).into_iter())),
+        }
+    }
+
+    #[pyo3(signature = (include_hidden=false))]
+    fn values(&self, include_hidden: bool) -> JsonnetObjectValues {
+        JsonnetObjectValues {
+            obj: self.clone(),
+            iter: RefCell::new(Box::new(self.0.fields_ex(include_hidden, true).into_iter())),
+        }
+    }
+
+    #[pyo3(signature = (include_hidden=false))]
+    fn items(&self, include_hidden: bool) -> JsonnetObjectItems {
+        JsonnetObjectItems {
+            obj: self.clone(),
+            iter: RefCell::new(Box::new(self.0.fields_ex(include_hidden, true).into_iter())),
+        }
+    }
+
     #[pyo3(signature = (minified=true))]
     fn manifest_json(&self, minified: bool) -> PyResult<String> {
         execute_jsonnet(|| {
@@ -68,13 +109,119 @@ impl JsonnetObject {
     }
 }
 
+#[pyclass(unsendable)]
+pub struct JsonnetObjectKeys {
+    iter: RefCell<Box<dyn Iterator<Item = jrsonnet_evaluator::IStr>>>,
+}
+
+#[pymethods]
+impl JsonnetObjectKeys {
+    fn __iter__(slf: PyRef<'_, Self>) -> PyRef<'_, Self> {
+        slf
+    }
+
+    fn __next__(slf: PyRefMut<'_, Self>) -> Option<String> {
+        slf.iter
+            .borrow_mut()
+            .next()
+            .map(|key| key.as_str().to_owned())
+    }
+}
+
+#[pyclass(unsendable)]
+pub struct JsonnetObjectValues {
+    obj: JsonnetObject,
+    iter: RefCell<Box<dyn Iterator<Item = jrsonnet_evaluator::IStr>>>,
+}
+
+#[pymethods]
+impl JsonnetObjectValues {
+    fn __iter__(slf: PyRef<'_, Self>) -> PyRef<'_, Self> {
+        slf
+    }
+
+    fn __next__<'py>(
+        slf: PyRefMut<'py, Self>,
+        py: Python<'py>,
+    ) -> PyResult<Option<Bound<'py, PyAny>>> {
+        execute_jsonnet(|| {
+            let Some(key) = slf.iter.borrow_mut().next() else {
+                return Ok(None);
+            };
+
+            let obj = &slf.obj.0;
+            let value = obj
+                .get(key)
+                .map_err(jsonnet_error)?
+                .expect("iterating over keys, field exists");
+
+            jsonnet_to_py(py, value).map(Some)
+        })
+    }
+}
+
+#[pyclass(unsendable)]
+pub struct JsonnetObjectItems {
+    obj: JsonnetObject,
+    iter: RefCell<Box<dyn Iterator<Item = jrsonnet_evaluator::IStr>>>,
+}
+
+#[pymethods]
+impl JsonnetObjectItems {
+    fn __iter__(slf: PyRef<'_, Self>) -> PyRef<'_, Self> {
+        slf
+    }
+
+    fn __next__<'py>(
+        slf: PyRefMut<'py, Self>,
+        py: Python<'py>,
+    ) -> PyResult<Option<(String, Bound<'py, PyAny>)>> {
+        execute_jsonnet(|| {
+            let Some(key) = slf.iter.borrow_mut().next() else {
+                return Ok(None);
+            };
+
+            let obj = &slf.obj.0;
+            let value = obj
+                .get(key.clone())
+                .map_err(jsonnet_error)?
+                .expect("iterating over keys, field exists");
+
+            let pykey = key.as_str().to_string();
+            let pyvalue = jsonnet_to_py(py, value)?;
+
+            Ok(Some((pykey, pyvalue)))
+        })
+    }
+}
+
 /// TODO
-#[pyclass(unsendable, mapping)]
+#[pyclass(unsendable, sequence)]
 #[derive(Clone)]
 pub struct JsonnetArray(pub jrsonnet_evaluator::val::ArrValue);
 
 #[pymethods]
 impl JsonnetArray {
+    fn __len__(&self) -> usize {
+        self.0.len()
+    }
+
+    fn __contains__(&self, py: Python<'_>, object: Bound<'_, PyAny>) -> PyResult<bool> {
+        execute_jsonnet(|| {
+            let value = py_to_jsonnet(py, object)?;
+
+            for c in self.0.iter_lazy() {
+                let c = c.evaluate().map_err(jsonnet_error)?;
+
+                if jrsonnet_evaluator::val::equals(&value, &c).map_err(jsonnet_error)? {
+                    return Ok(true);
+                }
+            }
+
+            Ok(false)
+        })
+    }
+
     fn __getitem__<'py>(
         &self,
         py: Python<'py>,
@@ -90,6 +237,62 @@ impl JsonnetArray {
             };
 
             jsonnet_to_py(py, value)
+        })
+    }
+
+    fn __iter__(&self) -> JsonnetArrayIter {
+        JsonnetArrayIter {
+            array: self.clone(),
+            idx: 0,
+        }
+    }
+
+    #[pyo3(signature = (minified=true))]
+    fn manifest_json(&self, minified: bool) -> PyResult<String> {
+        execute_jsonnet(|| {
+            let preserve_order = true;
+            let fmt = if minified {
+                jrsonnet_evaluator::manifest::JsonFormat::minify(preserve_order)
+            } else {
+                jrsonnet_evaluator::manifest::JsonFormat::cli(2, preserve_order)
+            };
+
+            jrsonnet_evaluator::Val::Arr(self.0.clone())
+                .manifest(fmt)
+                .map_err(jsonnet_error)
+        })
+    }
+}
+
+#[pyclass(unsendable)]
+pub struct JsonnetArrayIter {
+    array: JsonnetArray,
+    idx: usize,
+}
+
+#[pymethods]
+impl JsonnetArrayIter {
+    fn __iter__(slf: PyRef<'_, Self>) -> PyRef<'_, Self> {
+        slf
+    }
+
+    fn __next__<'py>(
+        mut slf: PyRefMut<'py, Self>,
+        py: Python<'py>,
+    ) -> PyResult<Option<Bound<'py, PyAny>>> {
+        let slf = &mut slf;
+
+        execute_jsonnet(|| {
+            slf.array
+                .0
+                .get(slf.idx)
+                .map_err(jsonnet_error)?
+                .map(|value| {
+                    let pyvalue = jsonnet_to_py(py, value)?;
+                    slf.idx += 1;
+                    Ok(pyvalue)
+                })
+                .transpose()
         })
     }
 }
